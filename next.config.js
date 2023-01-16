eae8fd262cae8b0d6d7b25144a9b9a24adaaa182
Bilgekaan Yilmaz <bilgekaanyilmaz@192.168.1.6>
2023-01-16 13:38:07 +0300

    performance updates for videos

--- next.config.js
+++ next.config.js
@@ -1,10 +1,21 @@
 // next.config.js
-module.exports = {
+const { withPlugins } = require("next-compose-plugins");
+const withVideos = require("next-videos");
+
+// next.config.js
+// module.exports = {
+//   i18n: {
+//     locales: ["en", "de", "tr"],
+//     defaultLocale: "en",
+//   },
+// };
+
+module.exports = withPlugins([withVideos], {
   i18n: {
     locales: ["en", "de", "tr"],
     defaultLocale: "en",
   },
-};
+});
 
 // const withBundleAnalyzer = require("@next/bundle-analyzer")({
 //   enabled: process.env.ANALYZE === "true",
